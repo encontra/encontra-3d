34ee1182dc5a64d825b09cbde4a284277e98d661
jpvguerreiro <jpvguerreiro@gmail.com>
2015-02-03 18:35:48 +0000

    Suporta CORS; Melhorias no Suporte a STL. Funcionava com Binary, e agora funciona com ASCII; API REST esta preparada para receber uma string "ASCII". Trata como um File (temporario).

--- encontra-3d-core/src/main/java/pt/inevo/encontra/threed/model/io/ModelReaderStl.java
+++ encontra-3d-core/src/main/java/pt/inevo/encontra/threed/model/io/ModelReaderStl.java
@@ -11,11 +11,10 @@ import pt.inevo.encontra.threed.model.BufferedModel;
 import pt.inevo.encontra.threed.model.geoset.BufferedMesh;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.*;
 
-//Obj Wavefront
 class ModelReaderStl extends ModelReader {
 
 	public ModelReaderStl(String name, String extension) {
@@ -32,14 +31,16 @@ class ModelReaderStl extends ModelReader {
         String token;
         // At this time, I am assuming the file is properly formatted. Error handling can be added later
 
-        BufferedReader ins = new BufferedReader(new FileReader(filename));
-        sc = new Scanner(ins.readLine());
+
+        //BufferedReader ins = new BufferedReader(new FileReader(filename));
+        sc = new Scanner(new FileReader(filename));
+        //sc.useLocale(Locale.US);
         token = sc.next();
         if("solid".equalsIgnoreCase(token)){ // ASCII Format
             sc.close();
-            mesh = parseAscii(ins);
+            mesh = parseAscii(filename);
         }else { // Binary format
-            ins.close(); // To reopen the file with FileInputStream
+            sc.close(); // To reopen the file with FileInputStream
             mesh = parseBinary(filename);
         }
 
@@ -49,20 +50,12 @@ class ModelReaderStl extends ModelReader {
         return model;
 	}
 
-    /**
-     * Convert an array of 4 bytes to an int
-     * @param array the array to convert
-     * @return the array converted to an int
-     */
-    private static int bytesToInt(byte[] array)
-    {
-        // array is assumed to consist of 4 bytes
-
-        return (array[3] << 24) | (array[2] << 16) | (array[1] << 8) | array[0];
-    }
-
     private BufferedMesh parseBinary(String filename) throws IOException {
         BufferedMesh mesh = new BufferedMesh();
+
+        Map<String, Integer> added_vertices = new HashMap<String, Integer>();
+        int v_index = 0;
+        float n_index = 0;
 
         FileInputStream fis = new FileInputStream(filename);
         fis.skip(80); // The first 80 bytes are the header, which we don't need
@@ -75,28 +68,47 @@ class ModelReaderStl extends ModelReader {
         int facets = bytesToInt(buffer);
         // We are assuming little-endian
 
-
         for(int faceCount = 0; faceCount < facets; faceCount++){
-            fis.skip(12); // 12 bytes for the normals. We don't need these
+            //normals
+
+            fis.read(buffer);
+            float nx = bytesToFloat(buffer);
+            fis.read(buffer);
+            float ny = bytesToFloat(buffer);
+            fis.read(buffer);
+            float nz = bytesToFloat(buffer);
+
+            mesh.addNormal(nx,ny,nz);
+
+            List<Integer> v = new ArrayList<Integer>();
+            List<Integer> vt = new ArrayList<Integer>();
+            List<Float> vn = new ArrayList<Float>();
+
+            vn.add(n_index);
+            n_index++;
 
             // Next we have 3 floats representing a vertex (x, y, z)
             // This is repeated two more times
-
             for(int j = 0; j < 3; j++){
                 fis.read(buffer);
-                double x = (double) Float.intBitsToFloat(bytesToInt(buffer));
+                double vx = (double) bytesToFloat(buffer);
                 fis.read(buffer);
-                double y = (double) Float.intBitsToFloat(bytesToInt(buffer));
+                double vy = (double) bytesToFloat(buffer);
                 fis.read(buffer);
-                double z = (double) Float.intBitsToFloat(bytesToInt(buffer));
-                mesh.addVertex(x, y, z);
+                double vz = (double) bytesToFloat(buffer);
+
+                String vert = Double.toString(vx) + Double.toString(vy) + Double.toString(vz);
+
+                if (!added_vertices.containsKey(vert)) {
+                    added_vertices.put(vert, v_index);
+                    mesh.addVertex(vx, vy, vz);
+                    v.add(v_index);
+                    v_index++;
+                } else {
+                    v.add(added_vertices.get(vert));
+                }
             }
 
-            List<Integer> v = new ArrayList<Integer>();
-            for (int i = 0; i<3; i++)
-            {
-                v.add(3*faceCount + i);
-            }
             mesh.addFace(v);
 
             fis.skip(2);
@@ -109,44 +121,101 @@ class ModelReaderStl extends ModelReader {
         return mesh;
     }
 
-    public BufferedMesh parseAscii(BufferedReader ins) throws IOException {
+    public BufferedMesh parseAscii(String filename) throws IOException {
         BufferedMesh mesh = new BufferedMesh();
-        int faceCount = 0;
-        while(true){ // This loop will be broken with break statements
 
-            Scanner sc = new Scanner(ins.readLine()); // If the file is properly formatted, we don't have to worry about readLine() returning null
-            String token = sc.next();
-            if(("endsolid".equalsIgnoreCase(token)) || ("end".equalsIgnoreCase(token))){ // We've reached the end of the file
+        Map<String, Integer> added_vertices = new HashMap<String, Integer>();
+        int v_index = 0;
+        float n_index = 0;
+
+//        BufferedReader ins = new BufferedReader(new FileReader(filename));
+        Scanner sc = new Scanner(new FileReader(filename));
+        //sc.useLocale(Locale.US);
+
+        while(sc.hasNextLine()) {
+
+            if (sc.findInLine("solid") != null) {
+                sc.nextLine();
+                continue;
+            }
+
+
+            if ((sc.findInLine("endsolid") != null) || !sc.hasNext()) { // We've reached the end of the file
                 break;
-            }else{ // Implicitly: else if("facet".equals(token)) . This is the only other valid possibility
-                // The rest of the line is "normal" followed by three coordinates
-                // We don't need any of this
-                ins.readLine(); // The next line MUST be "outer loop"
-                // The next three lines are vertex statements
-                for(int i = 0; i < 3; i++){
-                    sc.close();
-                    sc = new Scanner(ins.readLine());
-                    sc.next(); // This token is always "vertex"
-                    // The three remaining tokens are the vertex coordinates
-                    double vx = (double) sc.nextFloat();
-                    double vy = (double) sc.nextFloat();
-                    double vz = (double) sc.nextFloat();
-                    mesh.addVertex(vx, vy, vz);
+
+            } else {
+
+                String text = sc.next();
+
+                if (text.compareTo("facet") == 0) {
+                    //normal
+                    text = sc.next();
+                    float nx = Float.parseFloat(sc.next());
+                    float ny = Float.parseFloat(sc.next());
+                    float nz = Float.parseFloat(sc.next());
+                    mesh.addNormal(nx, ny, nz);
+                    sc.nextLine();
+
+                    if (sc.findInLine("outer loop") != null) {
+                        //removes outer loop line
+                        sc.nextLine();
+                        List<Integer> v = new ArrayList<Integer>();
+                        List<Integer> vt = new ArrayList<Integer>();
+                        List<Float> vn = new ArrayList<Float>();
+
+                        vn.add(n_index);
+                        n_index++;
+
+                        while (sc.findInLine("vertex") != null) {
+                            double vx = Double.parseDouble(sc.next());
+                            double vy = Double.parseDouble(sc.next());
+                            double vz = Double.parseDouble(sc.next());
+                            String vert = Double.toString(vx) + Double.toString(vy) + Double.toString(vz);
+
+                            if (!added_vertices.containsKey(vert)) {
+                                added_vertices.put(vert, v_index);
+                                mesh.addVertex(vx, vy, vz);
+                                v.add(v_index);
+                                v_index++;
+                            } else {
+                                v.add(added_vertices.get(vert));
+                            }
+                            sc.nextLine();
+                        }
+
+                        mesh.addFace(v, vt, vn);
+                        if (sc.findInLine("endfacet") != null) {
+                            sc.nextLine();
+                        }
+                    }
                 }
-                ins.readLine(); // This line MUST be "endloop"
-                ins.readLine(); // This line MUST be "endfacet"
-                List<Integer> v = new ArrayList<Integer>();
-                for (int i = 0; i<3; i++)
-                {
-                    v.add(3*faceCount + i);
-                }
-                mesh.addFace(v);
-                faceCount++;
             }
         }
-        ins.close();
         return mesh;
 
     }
 
+
+    /**
+     * Convert an array of 4 bytes to an int
+     * @param array the array to convert
+     * @return the array converted to an int
+     */
+    private static int bytesToInt(byte[] array)
+    {
+        // array is assumed to consist of 4 bytes
+        int i = (array[3] << 24) | (array[2] << 16) | (array[1] << 8) | array[0];
+        return i;
+    }
+
+    /**
+     * Convert an array of 4 bytes to an int
+     * @param array the array to convert
+     * @return the array converted to an int
+     */
+    private static float bytesToFloat(byte[] array)
+    {
+        // array is assumed to consist of 4 bytes
+        return ByteBuffer.wrap(array).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+    }
 }
